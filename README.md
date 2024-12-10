5b2096eba1a8217473a20ca29c51f56f31ab5fde
rahul <raxhvl@users.noreply.github.com>
2024-12-10 10:39:07 +0000

    📄 docs: Setup

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Elixir 🧪
 
 A research effort to optimize gas consumption of the EVM.
+
+## Documentation
+Documentation built using [Vocs](https://vocs.dev). Review their [guide](https://vocs.dev/docs) to
+get started.
---
+++ vocs.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vocs";
+
+export default defineConfig({
+  title: "Elixir 🧪",
+  sidebar: [
+    {
+      text: "Analysis",
+      items: [
+        {
+          text: "EIPs",
+          link: "analysis/eips",
+        },
+      ],
+    },
+  ],
+});
